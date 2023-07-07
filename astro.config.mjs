296e176ec4c21b1ef3cd372bf1f18a0c109152e0
Laddge <dev.laddge@gmail.com>
2023-07-07 01:01:09 +0000

    add astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
-      applyBaseStyles: false,
+      applyBaseStyles: false
     }),
-  ],
+    compress(),
+  ]
 });
